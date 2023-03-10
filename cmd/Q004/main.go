cfe0d32fcb448fb517d77bbb0524b6c0b6235d56
joshua <kjkq26993@gaia.eonet.ne.jp>
2023-03-11 08:21:24 +0900

    Q004 modified

--- cmd/Q004/main.go
+++ cmd/Q004/main.go
@@ -8,76 +8,30 @@ import (
 	"strings"
 )
 
-var reader = bufio.NewReaderSize(os.Stdin, 1e6)
-
-func Nextint(a ...*int) {
-	l := strings.Split(Nextline(), " ")
-	for i, n := range a {
-		*n, _ = strconv.Atoi(l[i])
-	}
-}
-
-func Nextskicei(a *[]int) {
-	l := strings.Split(Nextline(), " ")
-	if len(l) == 1 && l[0] == "" {
-		(*a) = make([]int, 0)
-		return
-	}
-	(*a) = make([]int, len(l))
-	for i := 0; i < len(l); i++ {
-		(*a)[i], _ = strconv.Atoi(l[i])
-	}
-}
-
-func Nextline() string {
-	buffer := make([]byte, 0)
-	for {
-		line, isPrefix, err := reader.ReadLine()
-		if err != nil {
-			panic(err)
-		}
-		buffer = append(buffer, line...)
-		if !isPrefix {
-			break
-		}
-	}
-	return string(buffer)
-}
-
-func Sli(l, m int, def int) [][]int {
-	sl := make([][]int, l)
-	for i := 0; i < l; i++ {
-		sl[i] = make([]int, m)
-		for j := 0; j < m; j++ {
-			sl[i][j] = def
-		}
-	}
-	return sl
-}
-
 func main() {
-	var h, w int
-	Nextint(&h, &w)
-	sl := Sli(h, w, 0)
-	r := make([]int, h)
-	c := make([]int, w)
-	for i, _ := range sl {
-		Nextskicei(&sl[i])
-		for j := 0; j < len(sl[i]); j++ {
+	sc := bufio.NewScanner(os.Stdin)
+	writer := bufio.NewWriter(os.Stdout)
+	defer writer.Flush()
+	sc.Scan()
+	s := strings.Split(sc.Text(), " ")
+	h, _ := strconv.Atoi(s[0])
+	w, _ := strconv.Atoi(s[1])
+	var sl [2000][2000]int
+	var r [2000]int
+	var c [2000]int
+	for i := 0; i < h; i++ {
+		sc.Scan()
+		s := strings.Split(sc.Text(), " ")
+		for j := 0; j < w; j++ {
+			sl[i][j], _ = strconv.Atoi(s[j])
 			r[i] += sl[i][j]
 			c[j] += sl[i][j]
 		}
 	}
-	//fmt.Println(h, w)
-	//fmt.Println(sl)
-	//fmt.Println("r:", r)
-	//fmt.Println("c:", c)
-	re := Sli(h, w, 0)
 	for i := 0; i < h; i++ {
 		for j := 0; j < w; j++ {
-			re[i][j] = r[i] + c[j] - sl[i][j]
-			fmt.Print(re[i][j], " ")
+			fmt.Fprint(writer, r[i]+c[j]-sl[i][j], " ")
 		}
-		fmt.Print("\n")
+		fmt.Fprint(writer, "\n")
 	}
 }
